48510fa28169dd5359bd81e9457427dccbdb6a6d
Alex Brodie <abrodie77@hotmail.com>
2018-11-25 21:17:58 -0800

    Initial Commit

---
+++ BrickMosaicCL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BrickMosaicCL
+//
+//  Created by Alexander Brodie on 11/25/18.
+//  Copyright © 2018 Alexander Brodie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
